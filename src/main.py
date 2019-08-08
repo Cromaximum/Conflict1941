692c7cece257599f0297ce72e1a6ba03196c6395
mrhinelander <mrhinelander@dekaresearch.com>
2019-08-08 17:00:58 -0400

    new main menu

--- src/main.py
+++ src/main.py
@@ -1,24 +1,83 @@
 # coding=utf-8
+import sys
+import os
+from DE_infantry import *
+from util import *
 from world import *
 
-def print_test_squad_DE():
-	print_squad(create_new_squad_DE())
+#TODO
+#Loop nation selection if soviet selected and display error
+#Loop Char creation screen and display non valid input error
 
-def print_test_squad_RU():
-	print_squad(create_new_squad_RU())
 
-def print_test_platoon_DE():
-	print_platoon(create_new_platoon_DE())
+#Game Loop
+game = True
 
-def print_test_platoon_RU():
-	print_platoon(create_new_platoon_RU())
+#Germany = True
+#Soviet = False
+nation = ''
 
-def print_test_company_DE():
-	print_company(create_new_company_DE())
+main_char = create_new_Soldier_DE('Squad Leader')
 
-def print_test_company_RU():
-	print_company(create_new_company_RU())
 
-print_test_company_DE()
-print("")
-print_test_company_RU()
+ENDC = '\033[0m'
+WARNING = '\033[93m'
+
+
+def randomization_loop():
+	global main_char
+	main_char = create_new_Soldier_DE('Squad Leader')
+
+def char_creation():
+	global game
+	global main_char
+	clear()
+	print("[1] German")
+	print("[2] Soviet")
+	ans = raw_input("Option: ")
+	if(ans == '1'):
+		while(game):
+			clear()
+			print(print_soldier(main_char))
+			print("[1] Accept")
+			print("[2] Re-Roll")
+			ans = raw_input("Option: ")
+			if(ans == '1'):
+				game = False
+				clear()
+			elif(ans == '2'):
+				main_char = create_new_Soldier_DE('Squad Leader')
+			else:
+				warning_text("Input not valid")
+
+	elif(ans == '2'):
+		pass
+	else:
+		clear()
+		print("Input not valid")
+		
+
+
+while(game):
+	print("~~ Conflict 1941 ~~")
+	print("[1] New Game")
+	print("[2] Credits")
+	print("[3] Exit Game")
+	ans = raw_input("Option: ")
+	if(ans == '3'):
+		sys.exit()
+	elif(ans == '2'):
+		clear()
+		print("Author: Maxwell Rhinelander")
+		print("Version x.x.x")
+		raw_input("Press Any Key to Continue... ")
+		clear()
+	elif(ans == '1'):
+		char_creation()
+		clear()
+
+	else:
+		clear()
+		warning_text("Non-Valid Input")
+
+print("Prost!")
--- src/util.py
+++ src/util.py
@@ -3,6 +3,10 @@ import math
 import random
 import time
 import sys
+import os
+
+ENDC = '\033[0m'
+WARNING = '\033[93m'
 
 def sleep_seconds(seconds):
     time.sleep(seconds)
@@ -12,3 +16,9 @@ def cool_text(text):
         sleep_seconds(.1)
         sys.stdout.write(char)
         sys.stdout.flush()
+
+def clear():
+	os.system('cls' if os.name == 'nt' else 'clear')
+
+def warning_text(string):
+	print((WARNING + string.upper() + ENDC))
