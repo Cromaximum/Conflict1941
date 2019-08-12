b1e42a96eca20d43f6319fa2e60101459dc5aba2
mrhinelander <mrhinelander@dekaresearch.com>
2019-08-12 15:51:06 -0400

    updated main

--- src/date.py
+++ src/date.py
@@ -1,51 +1,43 @@
-import random
+MONTHS = ['January','February','March','April', 'May',
+	'June','July','August','September','October',
+	'November', 'December']
 
+month_cnt = 6
+
+class date:
+    hour = 0
+    day = 22
+    month = MONTHS[month_cnt]
+    year = 1941
 
 def print_date_c(date):
     print("")
-    print(str(date.time) + ":00 " + date.month + " " + str(date.day) + " " + str(date.year))
+    print(str(date.hour) + ":00 " + date.month + " " + str(date.day) + " " + str(date.year))
     print("")
     print("-----------")
-    print("")
 
-def next_hour(date):
-    if date.time == 24:
-        if date.month == "June" and date.day == 30:
-            date.month = MONTHS[1]
-            date.day = 1
-            date.time = 0
-        elif date.month == "July" and date.day == 31:
-            date.month = MONTHS[2]
-            date.day = 1
-            date.time = 0
-        elif date.month == "August" and date.day == 31:
-            date.month = MONTHS[3]
-            date.day = 1
-            date.time = 0
-        elif date.month == "September" and date.day == 30:
-            date.month = MONTHS[4]
-            date.day = 1
-            date.time = 0
-        elif date.month == "October" and date.day == 31:
-            date.month = MONTHS[5]
-            date.day = 1
-            date.time = 0
-        elif date.month == "November" and date.day == 30:
-            date.month = MONTHS[5]
-            date.day = 1
-            date.time = 0
-    else:
-        date.time += 1
+def next_month(date):
+	global month_cnt
+	month_cnt += 1
+	date.month = MONTHS[month_cnt]
+	date.day = 1
 
-def random_starting_date(date):
-    date.month = random.choice(MONTHS)
-    date.day = random.randint(0,30)
-    date.time = random.randint(0,24)
-
-MONTHS = ['June','July','August','September','October','November']
-
-class date:
-    time = 0
-    day = 22
-    month = MONTHS[0]
-    year = 1941
+def iterate_by_hour(date):
+	global month_cnt
+	if(date.hour == 24):
+		date.hour = 1
+		if(date.day == 28 and date.month == 'February'):
+			next_month(date)
+		if(date.day == 30 and date.month == 'April' or date.month == 'June' 
+			or date.month == 'September' or date.month == 'November'):
+			next_month(date)
+		if(date.day == 31 and date.month == 'December'):
+			month_cnt = -1
+			next_month(date)
+		elif(date.day == 31):
+			next_month(date)
+		else:
+			date.day += 1
+	else:
+		date.hour += 1
+			
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 import sys
 import os
+import random
+from date import *
 from DE_infantry import *
 from util import *
 from world import *
@@ -9,9 +11,18 @@ from world import *
 #Loop nation selection if soviet selected and display error
 #Loop Char creation screen and display non valid input error
 
+#192.168.55.1
+
 
 #Game Loop
-game = True
+clear()
+main_menu = True
+main_interface = True
+char_selec = True
+
+
+main_squad = create_new_squad_DE()
+primary_date = date()
 
 #Germany = True
 #Soviet = False
@@ -29,55 +40,96 @@ def randomization_loop():
 	main_char = create_new_Soldier_DE('Squad Leader')
 
 def char_creation():
-	global game
+	global main_menu
+	global main_interface
 	global main_char
-	clear()
+	global char_selec
+	char_selec = True
 	print("[1] German")
 	print("[2] Soviet")
 	ans = raw_input("Option: ")
 	if(ans == '1'):
-		while(game):
+		clear()
+		while(char_selec):
 			clear()
 			print(print_soldier(main_char))
 			print("[1] Accept")
 			print("[2] Re-Roll")
 			ans = raw_input("Option: ")
 			if(ans == '1'):
-				game = False
+				char_selec = False
+				main_interface = True
+				main_menu = False
 				clear()
 			elif(ans == '2'):
 				main_char = create_new_Soldier_DE('Squad Leader')
 			else:
-				warning_text("Input not valid")
+				warning_text("!!! Non-Valid Input [' + ans + ']")
 
 	elif(ans == '2'):
-		pass
+		clear()
+		warning_text("Soviet's Not Currently Supported")
 	else:
 		clear()
 		print("Input not valid")
 		
+def main_interface_loop():
+	global main_interface
+	while(main_interface):
+		clear()
+		#sys.exit()
+		print_date_c(primary_date)
+		print('Squad Leader: ' + main_char.name)
+		print("-----------")
+		print("[1] Character Information...")
+		print("[2] Squad Information...")
+		print("[3] Wait...")
+		print("[4] Quit...")
+		print("-----------")
+		print("")
+		#iterate_by_hour(primary_date)
+		ans = raw_input("Option: ")
+		if(ans == '1'):
+			clear()
+			print_soldier(main_char)
+			raw_input("Press Any Key to Continue... ")
+		if(ans == '2'):
+			clear()
+			print('squad info')
+		if(ans == '3'):
+			pass
+		if(ans == '4'):
+			clear()
+			sys.exit()
+		else:
+			warning_text("!!! Non-Valid Input [" + ans + ']')
+			
 
-
-while(game):
+while(main_menu):
 	print("~~ Conflict 1941 ~~")
 	print("[1] New Game")
 	print("[2] Credits")
 	print("[3] Exit Game")
 	ans = raw_input("Option: ")
 	if(ans == '3'):
+		clear()
 		sys.exit()
 	elif(ans == '2'):
 		clear()
 		print("Author: Maxwell Rhinelander")
-		print("Version x.x.x")
+		print("Version x.X.x")
 		raw_input("Press Any Key to Continue... ")
 		clear()
 	elif(ans == '1'):
-		char_creation()
 		clear()
+		while(char_selec):
+			char_creation()
+		while(main_interface):
+			main_interface_loop()
 
 	else:
 		clear()
-		warning_text("Non-Valid Input")
+		warning_text("!!! Non-Valid Input [" + ans + ']')
 
-print("Prost!")
+
+
